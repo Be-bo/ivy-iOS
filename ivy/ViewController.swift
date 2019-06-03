8a35a769a450865b24eac0d28c309fd3fd8f5998
Bebo <rfiker@ucalgary.ca>
2019-06-02 19:37:21 -0600

    Initial Commit

---
+++ ivy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ivy
+//
+//  Created by Robert on 2019-06-02.
+//  Copyright © 2019 ivy social network. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
