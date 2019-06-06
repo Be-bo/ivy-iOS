57e6f0c7255e920cef361af024e3b1c7a741839d
pauldan24 <32079319+pauldan24@users.noreply.github.com>
2019-06-05 20:23:13 -0600

    Reg1UserEmail base front end

--- ivy/LoginRegister/Reg1UserEmail.swift
+++ ivy/LoginRegister/Reg1UserEmail.swift
@@ -2,8 +2,20 @@
 //  Reg1UserEmail.swift
 //  ivy
 //
-//  Created by Robert on 2019-06-05.
+//  Created by Paul Dan on 2019-06-05.
 //  Copyright © 2019 ivy social network. All rights reserved.
 //
 
 import Foundation
+import UIKit
+
+class Reg1UserEmail: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    
+}
