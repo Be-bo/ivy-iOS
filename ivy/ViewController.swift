f16479980508cd265c72ecc7d4b009519b08964e
Bebo <rfiker@ucalgary.ca>
2019-06-05 18:06:25 -0600

    nuhbruh

--- ivy/ViewController.swift
+++ ivy/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        var brah = "yah"
+        var brah = "nuhbruh"
     }
 
 
