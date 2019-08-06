600a2314d7eff6da276e3fd4a696f6b1be2cd242
pauldan24 <32079319+pauldan24@users.noreply.github.com>
2019-08-05 18:48:15 -0600

    Add participants, send and recieve messages, order of conversation, begin working on bolding of chats that are unread

--- ivy/Chat/ChatRoom.swift
+++ ivy/Chat/ChatRoom.swift
@@ -33,14 +33,32 @@ class ChatRoom: UIViewController, UITableViewDelegate, UITableViewDataSource{
         super.viewDidLoad()
         
         
-        print("THIS USER PROFILE", self.thisUserProfile)
+//        print("THIS USER PROFILE", self.thisUserProfile)
         self.startListeningToChangesInThisConversation()
-        
-        
+
         
     }
     
- 
+
+
+
+//called every single time a segway is called
+override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    let vc = segue.destination as! addParticipantPopUPViewController
+    vc.thisUserProfile = self.thisUserProfile   //pass the user profile object
+    vc.thisConversation = self.thisConversation
+}
+
+    
+    @IBAction func onClickAddParticipants(_ sender: Any) {
+        self.performSegue(withIdentifier: "addParticipantSegue" , sender: self) //pass data over to
+
+//        let popovervc = UIStoryboard(name:"Main", bundle: nil).instantiateViewController(withIdentifier: "addParticipantsPopup") as! addParticipantPopUPViewController
+//        self.addChild(popovervc)    //add view controller for our pop up viewto our current view controller
+//        popovervc.view.frame = self.view.frame
+//        self.view.addSubview(popovervc.view)
+//        popovervc.didMove(toParent: self)
+    }
     
     //when the user clicks the send message button
     @IBAction func onClickSendMessage(_ sender: Any) {
@@ -57,27 +75,26 @@ class ChatRoom: UIViewController, UITableViewDelegate, UITableViewDataSource{
             message["author_last_name"] = self.thisUserProfile["last_name"]
             message["author_id"] = self.thisUserProfile["id"] as! String
             message["conversation_id"] = self.thisConversation["id"] as! String
-            message["creation_time"] =  String(CACurrentMediaTime() * 1000)    //seconds * 1000 = milliseconds
+            message["creation_time"] =  Date().millisecondsSince1970   //seconds * 1000 = milliseconds
             message["message_text"] = inputMessage
             message["is_text_only"] = true
             message["file_reference"] = ""
             message["id"] =  NSUUID().uuidString
             
-            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-            print("message object", message)
-            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+
         
             baseDatabaseReference.collection("conversations").document(thisConversation["id"] as! String).collection("messages").document(message["id"] as! String).setData(message)
-            baseDatabaseReference.collection("conversations").document(thisConversation["id"] as! String).updateData(["last_message_millis": CLong(message["creation_time"] as! String) ])
+            baseDatabaseReference.collection("conversations").document(thisConversation["id"] as! String).updateData(["last_message_millis": message["creation_time"] as! Int64  ])
             baseDatabaseReference.collection("conversations").document(thisConversation["id"] as! String).updateData(["last_message": message["message_text"] as! String])
             baseDatabaseReference.collection("conversations").document(thisConversation["id"] as! String).updateData(["last_message_author": message["author_id"] as! String])
             baseDatabaseReference.collection("conversations").document(thisConversation["id"] as! String).updateData(["message_count": self.messages.count + 1])
             
             let thisUserPos = locateUser(id: thisUserProfile["id"] as! String) //get the position of the user in the array of participants to modify
-            if (thisUserPos != 1) {
-                var lastMsgCounts = thisConversation["last_message_counts"] as! Array<CLong>
+            
+            if (thisUserPos != -1) {
+                var lastMsgCounts = thisConversation["last_message_counts"] as? [CLong]
                 if(lastMsgCounts != nil) {
-                    lastMsgCounts[thisUserPos] = self.messages.count + 1
+                    lastMsgCounts![thisUserPos] = self.messages.count + 1
                     self.baseDatabaseReference.collection("conversations").document(self.thisConversation["id"] as! String).updateData(["last_message_counts": lastMsgCounts])
                 }
             }
@@ -100,10 +117,10 @@ class ChatRoom: UIViewController, UITableViewDelegate, UITableViewDataSource{
             
             if (snapshot.exists) {
                 self.thisConversation = snapshot.data()!
-                print("self.thisconversation", self.thisConversation)
+//                print("self.thisconversation", self.thisConversation)
                 
                 if(self.firstDataAquisition) {
-                    print("first data acquisition ")
+//                    print("first data acquisition ")
                     self.startRetrievingMessages()
                     self.firstDataAquisition = false
                 }
@@ -142,16 +159,17 @@ class ChatRoom: UIViewController, UITableViewDelegate, UITableViewDataSource{
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print("self.messages.count", self.messages.count)
         return self.messages.count
     }
     
     // called for every single cell thats displayed on screen/on reload
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
+        //called for each cell but will only update on the last index of messages since we only want it to update when the users "read" (loaded cell) the last message
+        updateLastSeenMessage()
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "ConversationCell", for: indexPath) as! ConversationCell
         let lastMessage = self.messages[indexPath.row]["message_text"] as! String
-        print("index path.row", indexPath.row)
         let lastMessageSenderID = self.messages[indexPath.row]["author_id"] as! String //the author of the last message that was sent
         var lastMessageAuthor = ""
         var authorProfilePicLoc = ""    //storage lcoation the profile pic is at
@@ -163,7 +181,9 @@ class ChatRoom: UIViewController, UITableViewDelegate, UITableViewDataSource{
             if let document = document, document.exists {
                 
                 lastMessageAuthor =  document.get("first_name") as! String //first name of last message author
-                authorProfilePicLoc = document.get("profile_picture") as! String //location of profile pic in storage
+                authorProfilePicLoc = "userimages/" + (document.get("id") as! String) + "/preview.jpg"
+
+//                authorProfilePicLoc = document.get("profile_picture") as! String //location of profile pic in storage
                 
                 // Create a storage reference from our storage service
                 let storageRef = self.baseStorageReference.reference()
@@ -186,10 +206,13 @@ class ChatRoom: UIViewController, UITableViewDelegate, UITableViewDataSource{
             }
         }
         
+
         
         return cell
     }
     
+    
+    // used to located the index of the conversation from the activeChats array
     func locateUser(id: String) -> Int {
         var position = 0
         var participants = [String]()
@@ -199,20 +222,51 @@ class ChatRoom: UIViewController, UITableViewDelegate, UITableViewDataSource{
                 position = index    //now I have the correct index corresponding to this specific modified chat from all the chats
             }
         }
-        print("position to modify", position)
         return position
     }
+    
+    
+    //update the last seen message count for this user for this conversations but only once we've loaded all its messages
+    func updateLastSeenMessage() {
 
-//    //function used to located the index of the conversation from the activeChats array
-//    func locateIndexOfConvo (id:String) -> Int {
-//        var position = 0
-//        for (index, chat) in self.activeChats.enumerated(){    //for every chat the user is part of
-//            if(id == chat["id"] as! String){  //if the chat has the same id as the modifiedID passed in
-//                position = index    //now I have the correct index corresponding to this specific modified chat from all the chats
-//            }
-//        }
-//        return position
-//    }
+        if(self.messages.count >= self.thisConversation["message_count"] as! CLong){    //if we have more messages then the amount thats been seen
+            var counts = self.thisConversation["last_message_counts"] as? [CLong]
+            let participants = self.thisConversation["participants"] as? [String]
+            
+            //make sure we actually retrieved the data
+            if(participants != nil && counts != nil){
+                for (index, participant) in participants!.enumerated(){    //for every chat the user is part of
+                    if(self.thisUserProfile["id"] as? String == participant){  //if the chat has the same id as the modifiedID passed in
+                        counts?[index] = (self.messages.count)
+                        //update the array in the db to contain the correct amount of messages now seen by this user
+                        self.baseDatabaseReference.collection("conversations").document(self.thisConversation["id"] as! String).updateData([
+                            "last_message_counts": counts,
+                        ]) { err in
+                            if let err = err {
+                                print("Error updating document: \(err)")
+                            } else {
+                                print("Document successfully updated")
+                            }
+                        }
+                        break
+                    }
+                }
+                
+            }
+            
+        }
+        
+        
+    }
 
+}
 
+extension Date {
+    var millisecondsSince1970:Int64 {
+        return Int64((self.timeIntervalSince1970 * 1000.0).rounded())
+    }
+    
+    init(milliseconds:Int64) {
+        self = Date(timeIntervalSince1970: TimeInterval(milliseconds) / 1000)
+    }
 }
