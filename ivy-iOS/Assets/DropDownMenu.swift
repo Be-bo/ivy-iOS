b720a9d3172df33488671bbc322ed3001c988a96
Zahra G <ghavasiehz@gmail.com>
2020-08-14 10:17:18 -0600

    Working on DropDown Menu. Need to adda scrollview

---
+++ ivy-iOS/Assets/DropDownMenu.swift
@@ -0,0 +1,62 @@
+//
+//  DropDownMenu.swift
+//  ivy-iOS
+//
+//  Created by Zahra Ghavasieh on 2020-08-13.
+//  Copyright © 2020 ivy. All rights reserved.
+//
+
+import SwiftUI
+
+struct DropDownMenu: View {
+    @State var expand = false
+    @State var selected: String? = nil
+    
+    // Provide these arguments
+    var list: [String]
+    var hint: String = "Expand"
+    var background: Color? = nil
+    
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text(self.selected ?? hint)
+                    .fontWeight(.bold)
+                Image (systemName: expand ? "chevron.up" : "chevron.down")
+                    .resizable()
+                    .frame(width: 13, height: 6)
+            }
+            .onTapGesture {
+                self.expand.toggle()
+            }
+            
+            if expand {
+                ForEach(self.list, id: \.self) { item in
+                    Button(action: {
+                        self.expand.toggle()
+                        self.selected = item
+                    }) {
+                        Text(item).padding()
+                    }.foregroundColor(.black)
+                }
+            }
+            
+        }
+        .padding()
+        .background(background)
+        .cornerRadius(15)
+        .animation(.spring())
+        
+    }
+}
+
+struct DropDownMenu_Previews: PreviewProvider {
+    static var previews: some View {
+        DropDownMenu(
+            list: (1...10).map{"Item \($0)"},
+            hint: "Choose a Number",
+            background: Color.gray
+        )
+    }
+}
--- ivy-iOS/LoginSignup/View/StudentSignup.swift
+++ ivy-iOS/LoginSignup/View/StudentSignup.swift
@@ -19,56 +19,55 @@ struct StudentSignup: View {
     @State private var selectedFrameworkIndex = 0
 
     var body: some View {
-        NavigationView {
-            Form {
-                Section {
-                    VStack(spacing: 15){
-                        
-                        Logo()
-                        
-                        TextField("Email", text: $email)
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
-                        
-                        TextField("Password", text: $password)
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
-                        
-                        TextField("Confirm Password", text: $confirmPassword)
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
-                                        
-                        
-                      
-                        Picker(selection: $selectedFrameworkIndex, label: Text("Favorite Frameword")) {
-                            ForEach(0 ..< frameworks.count) {
-                                Text(self.frameworks[$0])
-                            }
-                        }
-                        //.padding(.bottom, 30.0)
-                        
-                        Button(action: {
-                            //self.loginVM.attemptLogin()
-                        }){
-                            Text("Sign Up")
-                        }
-                        //.disabled(!loginVM.inputOk() || loginVM.waitingForResult) //button is disabled either when input not ok or when we're waiting for a Firebase result
-                        //.buttonStyle(StandardButtonStyle(disabled: !loginVM.inputOk())) //setting button style where background color changes based on if input is ok
-                        //.onReceive(loginVM.viewDismissalModePublisher) { shouldDismiss in //when shouldDismiss changes to true, dismiss this sheet
-                          //  if shouldDismiss {
-                            //    self.presentationMode.wrappedValue.dismiss()
-                            //} else {
-                              //  self.errorText = "Your email or password is invalid."
-                            //}
-                        //}
-                        
-                        Spacer()
-                    }
-                    .padding(.horizontal, 30.0)
-                }
-            }
-            .background(Gradient())
-        
+        VStack(spacing: 15){
             
+            Logo()
+            
+            TextField("Email", text: $email)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            TextField("Password", text: $password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            TextField("Confirm Password", text: $confirmPassword)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                            
+            
+            
+            
+            
+            
+            /*Picker(selection: $selectedFrameworkIndex, label: Text("Favorite Frameword")) {
+                ForEach(0 ..< frameworks.count) {
+                    Text(self.frameworks[$0])
+                }
+            } */
+            //.padding(.bottom, 30.0)
+            
+            
+            //DropDownMenu([1])
+            
+            
+            
+            Button(action: {
+                //self.loginVM.attemptLogin()
+            }){
+                Text("Sign Up")
+            }
+            //.disabled(!loginVM.inputOk() || loginVM.waitingForResult) //button is disabled either when input not ok or when we're waiting for a Firebase result
+            //.buttonStyle(StandardButtonStyle(disabled: !loginVM.inputOk())) //setting button style where background color changes based on if input is ok
+            //.onReceive(loginVM.viewDismissalModePublisher) { shouldDismiss in //when shouldDismiss changes to true, dismiss this sheet
+              //  if shouldDismiss {
+                //    self.presentationMode.wrappedValue.dismiss()
+                //} else {
+                  //  self.errorText = "Your email or password is invalid."
+                //}
+            //}
+            
+            Spacer()
         }
-        .navigationBarTitle("Degrees")
+        .padding(.horizontal, 30.0)
+        .background(Gradient())
     }
 }
 
