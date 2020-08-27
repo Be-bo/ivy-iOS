859e75eb84c0128ad5a63587e596e2633059df41
Zahra G <ghavasiehz@gmail.com>
2020-08-26 18:18:38 -0600

    Email Verification check added to login (fix #5)

--- ivy-iOS/LoginSignup/ViewModel/LoginViewModel.swift
+++ ivy-iOS/LoginSignup/ViewModel/LoginViewModel.swift
@@ -6,14 +6,18 @@
 //  Copyright © 2020 ivy. All rights reserved.
 //
 
+import SwiftUI
 import Foundation
 import Firebase
 import Combine
 
 class LoginViewModel: ObservableObject{
+    
     @Published var email = ""
     @Published var password = ""
     @Published var waitingForResult = false
+    @Published var errorText = ""
+    
     
     var viewDismissalModePublisher = PassthroughSubject<Bool, Never>() //allows us to dismiss LoginView when shouldDismissView changes to true
     private var shouldDismissView = false {
@@ -33,15 +37,24 @@ class LoginViewModel: ObservableObject{
     func attemptLogin() { //try to log in (async action Firebase)
         waitingForResult = true
         Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
-            if(error == nil){
+            
+            if (error == nil && result != nil && result!.user.isEmailVerified) { // all g
+                self.errorText = ""
                 print("Signed in!")
-                self.waitingForResult = false
                 self.shouldDismissView = true
-            }else{
-                self.waitingForResult = false
-                self.shouldDismissView = false
-                print("Error signing in with Auth.")
             }
+            else {
+                if (result != nil && !result!.user.isEmailVerified) { // Email not verified yet
+                    self.errorText = "Email not verified yet!"
+                }
+                else { // wrong credentials
+                    self.errorText = "Login failed, invalid email or password."
+                    print(error ?? "")
+                }
+                print(self.errorText)
+                self.shouldDismissView = false
+            }
+            self.waitingForResult = false
         }
     }
 }
